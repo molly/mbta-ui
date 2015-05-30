01d697e6d594d7499ec0b0f13555502923810558
Molly White <molly.white5@gmail.com>
2015-05-30 01:06:59 -0400

    Require both station and network data

--- coffee/main.coffee
+++ coffee/main.coffee
@@ -1,36 +1,37 @@
 (($) ->
-  plotStations = () ->
-    d3.json("dist/data/station-nodes.json", (err, data) ->
-      keys = _.keys(data)
-      stations = []
-      _.each keys, (k) =>
-        stations.push
-          x_axis: data[k][0]
-          y_axis: data[k][1]
-          radius: .05
-          color: "#ccc"
+  plotStations = (err, station_data, network_data) ->
+    keys = _.keys(station_data)
+    stations = []
+    _.each keys, (k) =>
+      stations.push
+        x_axis: station_data[k][0]
+        y_axis: station_data[k][1]
+        radius: .05
+        color: "#ccc"
 
-      max_x = _.max(stations, (s) -> s.x_axis).x_axis + 2
-      max_y = _.max(stations, (s) -> s.y_axis).y_axis + 2
-      width = $("body").width()
-      height = $("body").height()
+    max_x = _.max(stations, (s) -> s.x_axis).x_axis + 2
+    max_y = _.max(stations, (s) -> s.y_axis).y_axis + 2
+    width = $("body").width()
+    height = $("body").height()
 
-      svgContainer = d3.select(".map").append("svg")
-        .attr("viewBox", "0 0 #{max_x} #{max_y}")
-        .attr("width", width)
-        .attr("height", height)
+    svgContainer = d3.select(".map").append("svg")
+      .attr("viewBox", "0 0 #{max_x} #{max_y}")
+      .attr("width", width)
+      .attr("height", height)
 
-      stations = svgContainer.selectAll(".station")
-        .data(stations)
-        .enter()
-        .append("circle")
+    stations = svgContainer.selectAll(".station")
+      .data(stations)
+      .enter()
+      .append("circle")
 
-      stationAttributes = stations
-        .attr("cx", (d) -> d.x_axis + 1)
-        .attr("cy", (d) -> d.y_axis + 1)
-        .attr("r", (d) -> d.radius )
-        .style("fill", (d) -> d.color )
-    );
+    stationAttributes = stations
+      .attr("cx", (d) -> d.x_axis + 1)
+      .attr("cy", (d) -> d.y_axis + 1)
+      .attr("r", (d) -> d.radius )
+      .style("fill", (d) -> d.color )
 
-  plotStations()
+  queue()
+    .defer(d3.json, "dist/data/station-nodes.json")
+    .defer(d3.json, "dist/data/station-network.json")
+    .await(plotStations)
 ) jQuery, d3, _
--- dist/scripts/main.js
+++ dist/scripts/main.js
@@ -1,40 +1,38 @@
 (function($) {
   var plotStations;
-  plotStations = function() {
-    return d3.json("dist/data/station-nodes.json", function(err, data) {
-      var height, keys, max_x, max_y, stationAttributes, stations, svgContainer, width;
-      keys = _.keys(data);
-      stations = [];
-      _.each(keys, (function(_this) {
-        return function(k) {
-          return stations.push({
-            x_axis: data[k][0],
-            y_axis: data[k][1],
-            radius: .05,
-            color: "#ccc"
-          });
-        };
-      })(this));
-      max_x = _.max(stations, function(s) {
-        return s.x_axis;
-      }).x_axis + 2;
-      max_y = _.max(stations, function(s) {
-        return s.y_axis;
-      }).y_axis + 2;
-      width = $("body").width();
-      height = $("body").height();
-      svgContainer = d3.select(".map").append("svg").attr("viewBox", "0 0 " + max_x + " " + max_y).attr("width", width).attr("height", height);
-      stations = svgContainer.selectAll(".station").data(stations).enter().append("circle");
-      return stationAttributes = stations.attr("cx", function(d) {
-        return d.x_axis + 1;
-      }).attr("cy", function(d) {
-        return d.y_axis + 1;
-      }).attr("r", function(d) {
-        return d.radius;
-      }).style("fill", function(d) {
-        return d.color;
-      });
+  plotStations = function(err, station_data, network_data) {
+    var height, keys, max_x, max_y, stationAttributes, stations, svgContainer, width;
+    keys = _.keys(station_data);
+    stations = [];
+    _.each(keys, (function(_this) {
+      return function(k) {
+        return stations.push({
+          x_axis: station_data[k][0],
+          y_axis: station_data[k][1],
+          radius: .05,
+          color: "#ccc"
+        });
+      };
+    })(this));
+    max_x = _.max(stations, function(s) {
+      return s.x_axis;
+    }).x_axis + 2;
+    max_y = _.max(stations, function(s) {
+      return s.y_axis;
+    }).y_axis + 2;
+    width = $("body").width();
+    height = $("body").height();
+    svgContainer = d3.select(".map").append("svg").attr("viewBox", "0 0 " + max_x + " " + max_y).attr("width", width).attr("height", height);
+    stations = svgContainer.selectAll(".station").data(stations).enter().append("circle");
+    return stationAttributes = stations.attr("cx", function(d) {
+      return d.x_axis + 1;
+    }).attr("cy", function(d) {
+      return d.y_axis + 1;
+    }).attr("r", function(d) {
+      return d.radius;
+    }).style("fill", function(d) {
+      return d.color;
     });
   };
-  return plotStations();
+  return queue().defer(d3.json, "dist/data/station-nodes.json").defer(d3.json, "dist/data/station-network.json").await(plotStations);
 })(jQuery, d3, _);
